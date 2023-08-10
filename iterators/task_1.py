bfc8e25942c89fcbd5d5e2ecaf6ceb1e8f0a83b1
Maria <terentyevamarie@gmail.com>
2023-08-10 12:58:39 +0300

    changed iterators branch

--- iterators/task_1.py
+++
@@ -1,9 +0,0 @@
-def with_index(iterable, start = 0):
-    index = start
-    for i in iterable:
-        yield i, index
-        index+=1
-
-list_1 = [1, 2, 3, 70]
-for value, index in with_index(list_1):
-    print(f"Value : {value}, index : {index} ")
